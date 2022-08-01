1b02e3ae179c2167ec0e3d79e395014a13fbff83
Jeka2K <krukun1998@gmail.com>
2022-08-01 16:28:01 +0300

    Added docs and reviewed images folder

--- data/src/main/java/com/krykun/data/datasource/remote/impl/pagingsource/PopularMoviesPagingSource.kt
+++ data/src/main/java/com/krykun/data/datasource/remote/impl/pagingsource/PopularMoviesPagingSource.kt
@@ -5,10 +5,30 @@ import androidx.paging.PagingState
 import com.krykun.data.api.ApiService
 import com.krykun.data.model.remote.movies.MovieItemResponse
 
+/* It's a PagingSource that loads data from the network and returns a LoadResult.Page with the data and
+the next page number */
 class PopularMoviesPagingSource(
     private val apiService: ApiService,
 ) : PagingSource<Int, MovieItemResponse>() {
 
+    /**
+     * We are trying to load the next page of data from the API, and if we get a response, we return a
+     * LoadResult.Page object with the data, the previous key (which is null in this case), and the
+     * next key (which is the next page number). If we get an error, we return a LoadResult.Error
+     * object with the error
+     *
+     * @param params LoadParams<Int> - This is the page number that we want to load.
+     * @return LoadResult.Page(
+     *                 data = response.results as List<MovieItemResponse>,
+     *                 prevKey = null,
+     *                 nextKey = when {
+     *                     (response.page + 1) <= response.totalPages -> {
+     *                         response.page + 1
+     *                     }
+     *                     else -> null
+     *                 }
+     *             )
+     */
     override suspend fun load(params: LoadParams<Int>): LoadResult<Int, MovieItemResponse> {
         try {
             val nextPageNumber = params.key ?: 1
@@ -30,6 +50,14 @@ class PopularMoviesPagingSource(
         }
     }
 
+    /**
+     * > If the anchor position is not null, then get the closest page to the anchor position, and if
+     * the previous key is not null, then add 1 to it, otherwise, if the next key is not null, then
+     * subtract 1 from it
+     *
+     * @param state PagingState<Int, MovieItemResponse>
+     * @return The key of the next page to load.
+     */
     override fun getRefreshKey(state: PagingState<Int, MovieItemResponse>): Int? {
         return state.anchorPosition?.let { anchorPosition ->
             val anchorPage = state.closestPageToPosition(anchorPosition)
--- data/src/main/java/com/krykun/data/datasource/remote/impl/pagingsource/SearchPagingSource.kt
+++ data/src/main/java/com/krykun/data/datasource/remote/impl/pagingsource/SearchPagingSource.kt
@@ -5,11 +5,30 @@ import androidx.paging.PagingState
 import com.krykun.data.api.ApiService
 import com.krykun.data.model.remote.search.SearchItem
 
+/* It's a PagingSource that loads data from the API, and it's refresh key is the next page number */
 class SearchPagingSource(
     private val query: String,
     private val apiService: ApiService,
 ) : PagingSource<Int, SearchItem>() {
 
+    /**
+     * > We're trying to load the next page of data, and if we succeed, we return a `LoadResult.Page`
+     * object with the data, the previous page key (which is always null in our case), and the next
+     * page key (which is the current page number + 1, unless we're on the last page, in which case
+     * it's null). If we fail, we return a `LoadResult.Error` object with the exception
+     *
+     * @param params LoadParams<Int> - This is the page number that we want to load.
+     * @return LoadResult.Page(
+     *                 data = response.results as List<SearchItem>,
+     *                 prevKey = null,
+     *                 nextKey = when {
+     *                     (response.page + 1) <= response.totalPages -> {
+     *                         response.page + 1
+     *                     }
+     *                     else -> null
+     *                 }
+     *             )
+     */
     override suspend fun load(params: LoadParams<Int>): LoadResult<Int, SearchItem> {
         try {
             val nextPageNumber = params.key ?: 1
@@ -32,6 +51,14 @@ class SearchPagingSource(
         }
     }
 
+    /**
+     * If the anchor position is not null, then get the closest page to the anchor position, and if the
+     * previous key is not null, then add 1 to it, otherwise, if the next key is not null, then
+     * subtract 1 from it
+     *
+     * @param state PagingState<Int, SearchItem>
+     * @return The key of the next page to load.
+     */
     override fun getRefreshKey(state: PagingState<Int, SearchItem>): Int? {
         return state.anchorPosition?.let { anchorPosition ->
             val anchorPage = state.closestPageToPosition(anchorPosition)
